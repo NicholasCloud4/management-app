1293b6f1d365cc2045c2542cf6a5a6270a267128
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-30 10:00:35 -0500

    Created sidebar component

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Sidebar() {
+    return (
+        <aside>
+            <h2>Your Projects</h2>
+            <div>
+                <button>
+                    Add Project
+                </button>
+            </div>
+            <ul>
+                <li></li>
+            </ul>
+        </aside>
+    );
+}
