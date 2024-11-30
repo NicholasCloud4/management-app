6d404458f7af25ee4c16f5eac51655db21b579fe
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-30 10:53:26 -0500

    Styled the Buttons and Inputs

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 export default function Input({ label, textarea, ...props }) {
-    return (
-        <p>
-            <label htmlFor="">{label}</label>
-            {textarea ? <textarea {...props} /> : <input {...props} />}
-        </p>
 
-    )
+    const classes = "w-full p-2 border-2 rounded-md border-pokemon-orange bg-pokemon-grey text-pokemon-dark-grey focus:outline-none focus:border-pokemon-light-blue focus:bg-white transition-all duration-200"
+
+    return (
+        <p className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase text-pokemon-light-green">
+                {label}
+            </label>
+            {textarea ? (
+                <textarea
+                    className={classes}
+                    {...props}
+                />
+            ) : (
+                <input
+                    className={classes}
+                    {...props}
+                />
+            )}
+        </p>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
         'pokemon-light-blue': '#77BFFF', // Sky blue
         'pokemon-grey': '#F5F5F5', // Light grey for background
         'pokemon-dark-grey': '#A9A9A9', // For text contrast
+        'pokemon-light-green': '#77DD77',
+        'pokemon-orange': '#FFB347',
+        'pokemon-purple': '#C183C1'
       },
     },
   },
