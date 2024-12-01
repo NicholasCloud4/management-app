0a82fb8b11833bddbe2baef2aea1df130e9fd798
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-01 10:51:42 -0500

    Validating user input. Created a Modal component.

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,7 +1,11 @@
 import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
+
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -13,6 +17,13 @@ export default function NewProject({ onAdd }) {
 
 
         // validation ...
+
+        if (enteredTitle.trim() === "" || enteredDescription.trim() === "" || enteredDueDate.trim() === "") {
+            // show error Modal
+            modal.current.open()
+            return
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -22,26 +33,29 @@ export default function NewProject({ onAdd }) {
     }
 
     return (
-        <div className="w-[35rem] mt-16 bg-pokemon-grey p-8 rounded-lg shadow-lg">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li>
-                    <button className="text-pokemon-dark-grey hover:text-pokemon-light-red transition-colors duration-200">
-                        Cancel
-                    </button>
-                </li>
-                <li>
-                    <button
-                        className="px-6 py-2 rounded-md bg-pokemon-light-green text-white hover:bg-pokemon-orange hover:shadow-md transition-all duration-300"
-                        onClick={handleSave}>
-                        Save
-                    </button>
-                </li>
-            </menu>
-            <div>
-                <Input ref={title} type="text" label="Title" />
-                <Input ref={description} label="Description" textarea />
-                <Input ref={dueDate} type="date" label="Due Date" />
+        <>
+            <Modal ref={modal} buttonCaption="Close"><h2>Invalid Input</h2> <p>Please enter valid title, description and due date</p></Modal>
+            <div className="w-[35rem] mt-16 bg-pokemon-grey p-8 rounded-lg shadow-lg">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li>
+                        <button className="text-pokemon-dark-grey hover:text-pokemon-light-red transition-colors duration-200">
+                            Cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            className="px-6 py-2 rounded-md bg-pokemon-light-green text-white hover:bg-pokemon-orange hover:shadow-md transition-all duration-300"
+                            onClick={handleSave}>
+                            Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input ref={title} type="text" label="Title" />
+                    <Input ref={description} label="Description" textarea />
+                    <Input ref={dueDate} type="date" label="Due Date" />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
